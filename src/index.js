f035f36ed4763dbc87d35783d2a7d5d65020ccd5
Nima Shoghi <nimashoghi@gmail.com>
2019-12-26 11:20:34 -0500

    Removed native components

--- src/index.js
+++ src/index.js
@@ -1,265 +1,246 @@
 module.exports = {
   // Components
   get ActionBar() {
-    return require('./components').ActionBar;
+    return require("./components").ActionBar;
   },
   get ActionSheet() {
-    return require('./components').ActionSheet;
+    return require("./components").ActionSheet;
   },
   get AnimatedImage() {
-    return require('./components').AnimatedImage;
+    return require("./components").AnimatedImage;
   },
   get AnimatedScanner() {
-    return require('./components').AnimatedScanner;
+    return require("./components").AnimatedScanner;
   },
   get Avatar() {
-    return require('./components').Avatar;
+    return require("./components").Avatar;
   },
   get Badge() {
-    return require('./components').Badge;
+    return require("./components").Badge;
   },
   get Button() {
-    return require('./components').Button;
+    return require("./components").Button;
   },
   get Card() {
-    return require('./components').Card;
+    return require("./components").Card;
   },
   get Carousel() {
-    return require('./components').Carousel;
+    return require("./components").Carousel;
   },
   get Checkbox() {
-    return require('./components').Checkbox;
+    return require("./components").Checkbox;
   },
   get ColorPalette() {
-    return require('./components').ColorPalette;
+    return require("./components").ColorPalette;
   },
   get ColorPicker() {
-    return require('./components').ColorPicker;
+    return require("./components").ColorPicker;
   },
   get ColorSwatch() {
-    return require('./components').ColorSwatch;
+    return require("./components").ColorSwatch;
   },
   get ConnectionStatusBar() {
-    return require('./components').ConnectionStatusBar;
+    return require("./components").ConnectionStatusBar;
   },
   get Dialog() {
-    return require('./components').Dialog;
+    return require("./components").Dialog;
   },
   get FloatingButton() {
-    return require('./components').FloatingButton;
+    return require("./components").FloatingButton;
   },
   get FeatureHighlight() {
-    return require('./components').FeatureHighlight;
+    return require("./components").FeatureHighlight;
   },
   get Hint() {
-    return require('./components').Hint;
+    return require("./components").Hint;
   },
   get Image() {
-    return require('./components').Image;
+    return require("./components").Image;
   },
   get BaseInput() {
-    return require('./components').BaseInput;
+    return require("./components").BaseInput;
   },
   get TextArea() {
-    return require('./components').TextArea;
+    return require("./components").TextArea;
   },
   get TextField() {
-    return require('./components').TextField;
+    return require("./components").TextField;
   },
   get TextInput() {
-    return require('./components').TextInput;
+    return require("./components").TextInput;
   },
   get MaskedInput() {
-    return require('./components').MaskedInput;
+    return require("./components").MaskedInput;
   },
   get ListItem() {
-    return require('./components').ListItem;
+    return require("./components").ListItem;
   },
   get Notification() {
-    return require('./components').Notification;
+    return require("./components").Notification;
   },
   get PageControl() {
-    return require('./components').PageControl;
+    return require("./components").PageControl;
   },
   get PanningProvider() {
-    return require('./components').PanningProvider;
+    return require("./components").PanningProvider;
   },
   get PanGestureView() {
-    return require('./components').PanGestureView;
+    return require("./components").PanGestureView;
   },
   get PanListenerView() {
-    return require('./components').PanListenerView;
+    return require("./components").PanListenerView;
   },
   get PanDismissibleView() {
-    return require('./components').PanDismissibleView;
+    return require("./components").PanDismissibleView;
   },
   get PanResponderView() {
-    return require('./components').PanResponderView;
+    return require("./components").PanResponderView;
   },
   get Picker() {
-    return require('./components').Picker;
+    return require("./components").Picker;
   },
   get DateTimePicker() {
-    return require('./components').DateTimePicker;
+    return require("./components").DateTimePicker;
   },
   get ProgressBar() {
-    return require('./components').ProgressBar;
+    return require("./components").ProgressBar;
   },
   get Slider() {
-    return require('./components').Slider;
+    return require("./components").Slider;
   },
   get GradientSlider() {
-    return require('./components').GradientSlider;
+    return require("./components").GradientSlider;
   },
   get ColorSliderGroup() {
-    return require('./components').ColorSliderGroup;
+    return require("./components").ColorSliderGroup;
   },
 
   get Stepper() {
-    return require('./components').Stepper;
+    return require("./components").Stepper;
   },
   get TabBar() {
-    return require('./components').TabBar;
+    return require("./components").TabBar;
   },
   get TagsInput() {
-    return require('./components').TagsInput;
+    return require("./components").TagsInput;
   },
   get RadioButton() {
-    return require('./components').RadioButton;
+    return require("./components").RadioButton;
   },
   get RadioGroup() {
-    return require('./components').RadioGroup;
+    return require("./components").RadioGroup;
   },
   get SharedTransition() {
-    return require('./components').SharedTransition;
+    return require("./components").SharedTransition;
   },
   get StackAggregator() {
-    return require('./components').StackAggregator;
+    return require("./components").StackAggregator;
   },
   get Switch() {
-    return require('./components').Switch;
+    return require("./components").Switch;
   },
   get Text() {
-    return require('./components').Text;
+    return require("./components").Text;
   },
   get Toast() {
-    return require('./components').Toast;
+    return require("./components").Toast;
   },
   get TouchableOpacity() {
-    return require('./components').TouchableOpacity;
+    return require("./components").TouchableOpacity;
   },
   get Tour() {
-    return require('./components').Tour;
+    return require("./components").Tour;
   },
   get View() {
-    return require('./components').View;
+    return require("./components").View;
   },
   get Wizard() {
-    return require('./components').Wizard;
+    return require("./components").Wizard;
   },
   get WheelPickerDialog() {
-    return require('./components').WheelPickerDialog;
+    return require("./components").WheelPickerDialog;
   },
 
   // Assets
   get Assets() {
-    return require('./assets').default;
+    return require("./assets").default;
   },
 
   // Commons
   get BaseComponent() {
-    return require('./commons').BaseComponent;
+    return require("./commons").BaseComponent;
   },
   get PureBaseComponent() {
-    return require('./commons').PureBaseComponent;
+    return require("./commons").PureBaseComponent;
   },
   get SelectableComponent() {
-    return require('./commons').SelectableComponent;
+    return require("./commons").SelectableComponent;
   },
   get UIComponent() {
-    return require('./commons').UIComponent;
+    return require("./commons").UIComponent;
   },
   get forwardRef() {
-    return require('./commons').forwardRef;
+    return require("./commons").forwardRef;
   },
 
   // Helpers
   get AvatarHelper() {
-    return require('./helpers').AvatarHelper;
+    return require("./helpers").AvatarHelper;
   },
   get Constants() {
-    return require('./helpers').Constants;
+    return require("./helpers").Constants;
   },
   get DocsGenerator() {
-    return require('./helpers').DocsGenerator;
+    return require("./helpers").DocsGenerator;
   },
 
   // Services
   get LogService() {
-    return require('./services').LogService;
+    return require("./services").LogService;
   },
 
   // ScreenComponents
   get LoaderScreen() {
-    return require('./screensComponents').LoaderScreen;
+    return require("./screensComponents").LoaderScreen;
   },
   get Modal() {
-    return require('./screensComponents').Modal;
+    return require("./screensComponents").Modal;
   },
   get StateScreen() {
-    return require('./screensComponents').StateScreen;
-  },
-
-  // NativeComponents
-  get HighlighterOverlayView() {
-    return require('./nativeComponents').HighlighterOverlayView;
-  },
-  get SafeAreaSpacerView() {
-    return require('./nativeComponents').SafeAreaSpacerView;
-  },
-  get WheelPicker() {
-    return require('./nativeComponents').WheelPicker;
-  },
-  get SafeAreaInsetsManager() {
-    return require('./nativeComponents').SafeAreaInsetsManager;
-  },
-
-  // InteractableComponents
-  get Drawer() {
-    return require('./interactableComponents').Drawer;
+    return require("./screensComponents").StateScreen;
   },
 
   // Style
   get Colors() {
-    return require('./style').Colors;
+    return require("./style").Colors;
   },
   get BorderRadiuses() {
-    return require('./style').BorderRadiuses;
+    return require("./style").BorderRadiuses;
   },
   get Shadows() {
-    return require('./style').Shadows;
+    return require("./style").Shadows;
   },
   get Spacings() {
-    return require('./style').Spacings;
+    return require("./style").Spacings;
   },
   get Components() {
-    return require('./style').Components;
+    return require("./style").Components;
   },
   get ComponentsColors() {
-    return require('./style').ComponentsColors;
+    return require("./style").ComponentsColors;
   },
   get ThemeManager() {
-    return require('./style').ThemeManager;
+    return require("./style").ThemeManager;
   },
   get Typography() {
-    return require('./style').Typography;
+    return require("./style").Typography;
   },
   get AnimatableManager() {
-    return require('./style').AnimatableManager;
+    return require("./style").AnimatableManager;
   },
 
   // Incubator
   get Incubator() {
-    return require('./incubator');
+    return require("./incubator");
   }
 };
